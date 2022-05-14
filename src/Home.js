07ab049b72e6fec057992667291a880f51c1750d
ThushanDodanwela <dodanwelathushan@gmail.com>
2022-05-15 01:15:38 +0530

    Header DOne

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/711Y9Al9RNL._SX3000_.jpg"
+          alt="Banner"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDg_yezkHcL0B6F4ZcjMcJDX9ejBJkNr80",
+  authDomain: "clone-5f461.firebaseapp.com",
+  projectId: "clone-5f461",
+  storageBucket: "clone-5f461.appspot.com",
+  messagingSenderId: "809192693093",
+  appId: "1:809192693093:web:951289537c739bab78d58c",
+  measurementId: "G-VX9VGDRNG0",
+};
