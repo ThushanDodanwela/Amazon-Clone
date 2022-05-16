7126493f367e67afe90d4c20970d623db4d7d5e0
ThushanDodanwela <dodanwelathushan@gmail.com>
2022-05-16 23:52:02 +0530

    Login user History

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
+import react, { useEffect } from "react";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStatevalue } from "./StateProvider";
 
 function App() {
+  const [{}, disptch] = useStatevalue();
+
+  useEffect(() => {
+    //this will only run once when the app components load...
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+
+      if (authUser) {
+        //user just logged in / was logged in
+        disptch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //user logged out
+        disptch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="app">
